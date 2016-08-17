62381e0d44abe588bb3eee310be9552586236621
SHI-AR <COMPUTE.SGAZETTEDRAGONFLY@OUTLOOK.COM>
2016-08-17 09:49:27 -0500

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SHI-ARCONTEXTCONSIDERED")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Property of F046, 1763C")]
+[assembly: AssemblyProduct("SHI-ARCONTEXTCONSIDERED")]
+[assembly: AssemblyCopyright("Copyright © Property of F046, 1763C 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fdf8b43c-0583-44e1-82d4-0851dbb903f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SHI_ARCONTEXTCONSIDERED
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        int Click = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Click++;
+            switch (Click)
+            {
+                case 1:
+                    pictureBox1.Image = SHI_ARCONTEXTCONSIDERED.Properties.Resources.LILANDRA;
+                    break;
+                case 2:
+                    pictureBox1.Image = SHI_ARCONTEXTCONSIDERED.Properties.Resources.GALACTUS;
+                    break;
+                case 3:
+                    pictureBox1.Image = SHI_ARCONTEXTCONSIDERED.Properties.Resources.ETERNITY;
+                    break;
+                case 4:
+                    pictureBox1.Image = SHI_ARCONTEXTCONSIDERED.Properties.Resources.Nolgor_Subsector;
+                    break;
+                case 5:
+                    pictureBox1.Image = SHI_ARCONTEXTCONSIDERED.Properties.Resources.DRDOOM;
+                    break;
+                case 6:
+                    pictureBox1.Image = SHI_ARCONTEXTCONSIDERED.Properties.Resources.CAPITALONE;
+                    break;
+                case 7:
+                    pictureBox1.Image = SHI_ARCONTEXTCONSIDERED.Properties.Resources.VPROGRAM1;
+                    break;
+                case 8:
+                    pictureBox1.Image = SHI_ARCONTEXTCONSIDERED.Properties.Resources.VPROGRAM2;
+                    Click = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
